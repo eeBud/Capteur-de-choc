8eb04025c3eb0a80243c4ac540a0858112bb716a
eeBud <114733448+eeBud@users.noreply.github.com>
2023-01-19 13:38:40 +0100

    Add files via upload

---
+++ Identifiants.h
@@ -0,0 +1,10 @@
+
+String SSID ="SSID";
+String Mot_de_Passe ="MotDePasse";
+String IP_Jeedom = "AdresseIPJeedom";
+String API_Key_Jeedom = "CléAPIVirtuel";
+String ID_Choc_Jeedom = "IDVirtuelChoc";
+String ID_Ouverture_Jeedom = "IDVirtuelOuverture";
+int Timer_Veille_Secondes = 5;
+int Timer_Choc_Secondes = 4; 
+String URL = "http://#IP#/core/api/jeeApi.php?apikey=#APIKEY#&plugin=virtual&type=event&id=#ID#&value=#VALUE#";
